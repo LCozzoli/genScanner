3d16d1e63c8603d0a82dbaa4a383605ae0f9f72c
Xen <LCozzoli@users.noreply.github.com>
2021-03-25 13:39:14 +0100

    genScanner

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # genScanner
 A scanner for playRust plants seeds genetics
+
+## Requirements
+
+You need Python3 and pyautogui library.
+Once you installed Python39+, use `pip3 install pyautogui`.
+
+## Running the script
+
+Clone the repository, start a shell inside the folder, run `python .`
+
---
+++ __main__.py
@@ -0,0 +1,45 @@
+import pyautogui
+from time import sleep
+
+class GeneScanner:
+    def __init__(self):
+        self.genes = ['X', 'Y', 'G', 'W', 'H']
+        self.list = []
+    
+    def run(self):
+        while True:
+            self.update()
+            sleep(.5)
+
+    def update(self):
+        self.current = pyautogui.screenshot(region=(1160, 360, 285, 40))
+        self.scanAll()
+    
+    def searchFor(self, gene):
+        matches = []
+        lastMatch = -1
+        for coords in pyautogui.locateAll("genes/%s.png" % (gene), self.current, confidence=.8):
+            if lastMatch > 0 and coords.left - lastMatch < 30: 
+                continue
+            matches.append((gene, coords.left))
+            lastMatch = coords.left
+        return matches
+
+    def scanAll(self):
+        grab = []
+        for gene in self.genes:
+            grab += self.searchFor(gene)
+        reg = sorted(grab, key=lambda x: x[1])
+        if (len(reg) != 6): 
+            return
+        self.add(''.join(i[0] for i in reg))
+    
+    def add(self, genetic):
+        if genetic not in self.list:
+            print('New seed :', genetic)
+            self.list.append(genetic)
+            with open('list.txt', 'w') as fp:
+                fp.write('\n'.join('%s' % x for x in self.list))
+
+instance = GeneScanner()
+instance.run()
