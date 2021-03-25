865191178aea76645c19bee9092a27b9b8dba6e0
Xen <LCozzoli@users.noreply.github.com>
2021-03-25 15:35:41 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,5 +1,15 @@
 import pyautogui
+import winsound
+import os
 from time import sleep
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 class GeneScanner:
     def __init__(self):
@@ -18,7 +28,7 @@ class GeneScanner:
     def searchFor(self, gene):
         matches = []
         lastMatch = -1
-        for coords in pyautogui.locateAll("genes/%s.png" % (gene), self.current, confidence=.75):
+        for coords in pyautogui.locateAll(resource_path("./genes/%s.png" % (gene)), self.current, confidence=.7):
             if lastMatch > 0 and coords.left - lastMatch < 30: 
                 continue
             matches.append((gene, coords.left))
@@ -37,9 +47,11 @@ class GeneScanner:
     def add(self, genetic):
         if genetic not in self.list:
             self.list.append(genetic)
+            winsound.Beep(440, 200)
             print('New seed #%d:' % (len(self.list)), genetic)
             with open('list.txt', 'w') as fp:
                 fp.write('\n'.join('%s' % x for x in self.list))
 
 instance = GeneScanner()
+print('Running genScanner by SegFault..\n\nPlease ensure that you\'ve set your resolution to 1920x1080\nResult file will output to list.txt\n\n')
 instance.run()
